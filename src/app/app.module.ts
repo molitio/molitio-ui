e41e52a833e24285f3c9be9760d5f4d8d2cc5269
peterkollerlv <peterkollerlv@live.com>
2020-10-31 02:43:09 +0100

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './landing-page/landing-page.component';
-import { MolitioLogoComponent } from './molitio-logo/molitio-logo.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { MolitioLogoComponent } from './components/molitio-logo/molitio-logo.component';
+import { AboutComponent } from './components/about/about.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: LandingPageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '*', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
-    MolitioLogoComponent
+    MolitioLogoComponent,
+    NavigationBarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
