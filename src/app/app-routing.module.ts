40b0403997bc2476e49f996b5e273ec2db95f445
peterkollerlv <peterkollerlv@live.com>
2020-11-03 11:18:15 +0100

    logo positioned

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,9 @@ import { LandingPageComponent } from './components/landing-page/landing-page.com
 import { AboutComponent } from './components/about/about.component';
 import { MissionBoardComponent } from './components/mission-board/mission-board.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { enableDebugTools } from '@angular/platform-browser';
 
 const routes: Routes = [
-  { path: 'welcome', component: LandingPageComponent },
+  { path: '', component: LandingPageComponent },
   { path: 'about', component: AboutComponent },
   { path: 'mission-board', component: MissionBoardComponent },
   { path: '*', component: PageNotFoundComponent }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,8 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserAnimationsModule,
     MatMenuModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
