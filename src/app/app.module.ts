c4aa2b497068e96ab7abbdc1bb7627546113fffd
peterkollerlv <peterkollerlv@live.com>
2020-11-24 08:40:53 +0100

    added maps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
 import { LogoComponent } from './components/logo/logo.component';
-import { RouterModule } from '@angular/router';
 import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { MissionBoardComponent } from './components/mission-board/mission-board.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { MatMenuModule } from '@angular/material/menu';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
 import { MessagesComponent } from './components/messages/messages.component';
 import { CommunityHubComponent } from './components/community-hub/community-hub.component';
@@ -41,7 +41,8 @@ import { ResourceManagementComponent } from './components/resource-management/re
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    GoogleMapsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
