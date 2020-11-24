43b5a795d6818af6ee81b7623bfe979a74094c8a
peterkollerlv <peterkollerlv@live.com>
2020-11-24 09:02:56 +0100

    added map options

--- src/app/components/mission-board/mission-board.component.ts
+++ src/app/components/mission-board/mission-board.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'molitio-mission-board',
   templateUrl: './mission-board.component.html',
-  styleUrls: ['./mission-board.component.scss']
+  styleUrls: ['./mission-board.component.scss'],
 })
 export class MissionBoardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  mapsViewHeight: number;
+  zoom = 1;
+  options: google.maps.MapOptions = {
+    
   }
 
+  constructor() {
+    this.mapsViewHeight = 500;
+    this.getScreenHeight();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  getScreenHeight(event?) {
+    this.mapsViewHeight = window.innerHeight - 100;
+  }
+  ngOnInit(): void {}
 }
