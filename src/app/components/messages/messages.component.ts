b9ab234602228ebefcb08f086b9ad5aed7091bfd
peterkollerlv <peterkollerlv@live.com>
2020-11-26 02:04:34 +0100

    poc with amqp

--- src/app/components/messages/messages.component.ts
+++ src/app/components/messages/messages.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import * as Amqp from 'amqp-ts';
 
 @Component({
   selector: 'molitio-messages',
   templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.scss']
+  styleUrls: ['./messages.component.scss'],
 })
 export class MessagesComponent implements OnInit {
+  //probably going websockets and api
+  connection = new Amqp.Connection('amqp://localhost');
+  queue = this.connection.declareQueue('desideratium', { durable: false });
+  messages: string[] = [];
 
-  constructor() { }
+  getMessages = () => {
+    this.queue.activateConsumer((message) => {
+      this.messages.push(message.getContent().toString());
+    }, {noAck: true});
+  };
 
-  ngOnInit(): void {
+  constructor() {
+    this.getMessages();
   }
 
+  ngOnInit(): void {}
 }
